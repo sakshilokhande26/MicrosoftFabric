a7132f43811f8959dde1fc5f3ce9791ba9c02c18
Sakshi Lokhande <57286929+sakshilokhande26@users.noreply.github.com>
2026-02-25 15:38:42 +0530

    Delete ABC_Shared_DW.Warehouse directory

--- ABC_Shared_DW.Warehouse/Security/Tables/UserDivisionMapping.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [Security].[UserDivisionMapping] (
-
-	[UserID] int NOT NULL, 
-	[UserEmail] varchar(200) NOT NULL, 
-	[Division] varchar(50) NOT NULL, 
-	[FullName] varchar(100) NULL, 
-	[IsActive] bit NULL, 
-	[CreatedDate] date NULL
-);
